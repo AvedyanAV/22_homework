3c08126b4fa6a184b0617bf1a258d4d95def1234
Amayak Avedyan <Amajk577@mail.ru>
2026-01-06 10:29:44 +0300

    add migrations

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100, verbose_name='Категории')
+    description = models.CharField(max_length=300, verbose_name='Описание')
+
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
+        ordering = ['category_name',]
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.CharField(max_length=300, verbose_name='Описание')
+    picture = models.ImageField(upload_to='picture/', verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='product')
+    price = models.IntegerField(verbose_name='Цена')
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product_name
+
+    class Meta:
+        verbose_name = "продукт"
+        verbose_name_plural = "продукты"
+        ordering = ['product_name',]
--- config/settings.py
+++ config/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'homework',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -120,3 +124,6 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT =BASE_DIR / 'media'
